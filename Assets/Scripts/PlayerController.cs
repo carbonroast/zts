ac9be02ea382d83c73a98b6510a0d91e6223555e
carbonroast <zergling3@gmail.com>
2018-08-09 23:30:52 -0400

    raycast tile data

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
     public Camera cam;
 
@@ -23,15 +24,28 @@ public class PlayerController : MonoBehaviour {
             
             RaycastHit2D hit2d = Physics2D.GetRayIntersection(ray);
             Debug.DrawRay(ray.origin, ray.direction * 30, Color.red, 5);
+
+            //Click on Tilemap
             if(hit2d.collider != null)
             {
                 GridLayout grid = hit2d.transform.GetComponentInParent<GridLayout>();
-                Debug.Log("clicked - " + mousecoord + " " + hit2d.collider.name);
                 Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
                 Vector3Int position = grid.WorldToCell(worldPoint);
-                Debug.Log(position);
+                Vector2Int position2d = new Vector2Int(position.x, position.y);
+
+                string netid = hit2d.collider.GetComponent<NetworkIdentity>().netId.ToString();
+                SpriteTile tileClicked = SpriteTileManager.GetTile(position2d.ToString() + " " + netid);
+
+                Debug.Log(
+                    "name - " + tileClicked.name + 
+                    "location - " + tileClicked.location + 
+                    "buildable -" + tileClicked.buildable
+                );
+                //Debug.Log(position);
+                //Debug.Log("clicked - " + mousecoord + " " + hit2d.collider.name);
+
             }
-            if(Physics.Raycast(ray,out hit, 100))
+            if (Physics.Raycast(ray,out hit, 100))
             {
                 Debug.Log("hit - " + hit.transform.name);
             }
